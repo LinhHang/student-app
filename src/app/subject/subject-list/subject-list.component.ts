4d84d6d433c3cdbcee0db7c851e147370df0fada
namle0109 <nam.le@ntq-solution.com.vn>
2020-06-24 17:46:55 +0700

    #master update subject list view

--- src/app/subject/subject-list/subject-list.component.ts
+++ src/app/subject/subject-list/subject-list.component.ts
@@ -5,7 +5,35 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./subject-list.component.scss']
 })
 export class SubjectListComponent implements OnInit {
+  public subjects: Array<any>;
+  public totalPages: number;
+  public currentPage: number;
+  public page: number;
+
   constructor() {}
   ngOnInit(): void {
+    this.subjects = [
+      {
+        name: 'Duong loi Ho Chi Minh',
+        code: 'SB001',
+        credit_point: 3
+      },
+      {
+        name: 'Giai tich 2',
+        code: 'GT002',
+        credit_point: 3
+      },
+      {
+        name: 'Vat ly dai cuong 1',
+        code: 'VL001',
+        credit_point: 4
+      }
+    ];
+
+    this.totalPages = this.subjects.length;
+    this.currentPage = 1;
+    this.page = 1;
   }
+
+  public redirectCallback(page: number) {}
 }
